98c027a2089a7fa6ae1f5c8ffb90e0b3d80038d8
JacobGuiang <jacob.guiang1@gmail.com>
2022-08-01 16:44:36 -0500

    Add JavaScript to check if password inputs match

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const passA = document.getElementById("password");
-const passB = document.getElementById("confirm_password");
-const msg = document.getElementById("error-msg");
+const passA = document.getElementById('password');
+const passB = document.getElementById('confirm_password');
+const msg = document.getElementById('error-msg');
 
+function checkPass() {
+  if((passA.value.length != 0 && passB.value.length != 0) && passA.value == passB.value) {
+    passA.classList.remove('error');
+    passB.classList.remove('error');
+    msg.textContent = '';
+  } else {
+    passA.classList.add('error');
+    passB.classList.add('error');
+    msg.textContent = '* Passwords do not match';
+  }
+}
+
+passA.addEventListener('change', checkPass);
+passB.addEventListener('change', checkPass);
